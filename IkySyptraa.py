212699b459fb189d41a34f25183a996a25a101a7
IkySyptraa <142761687+IkySyptraa@users.noreply.github.com>
2025-02-01 02:39:12 +0700

    Create IkySyptraa.py

---
+++ IkySyptraa.py
@@ -0,0 +1,10 @@
+echo "# IkySyptraa" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/IkySyptraa/IkySyptraa.git
+git push -u origin main￼Enter
+git remote add origin https://github.com/IkySyptraa/IkySyptraa.git
+git branch -M main
+git push -u origin main
